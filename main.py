8915212ca793fbc4c0fbbce28d854e961dc0935e
Joe Wang <joewang@box.com>
2018-05-28 15:26:45 -0700

    Use chapter info extracted by ytdl instead of using homegrown regex

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import youtube_dl
 import tempfile
-import pydub
 import os
 import re
+import json
+from pydub import AudioSegment
+import argparse
 
 class MyLogger(object):
     def debug(self, msg):
@@ -15,22 +17,6 @@ class MyLogger(object):
         print(msg)
 
 
-# tmpdir=tempfile.mkdtemp()
-# print(tmpdir)
-tmpdir=os.getcwd()
-print(tmpdir)
-
-def downloadAudio(url):
-    ydl_opts = {
-        'writedescription': True,
-        'writeinfojson': True,
-        'outtmpl': tmpdir + '/' + 'AUDIO.%(ext)s',
-        'logger': MyLogger(),
-        'format': 'bestaudio[ext=m4a]'
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
 
 # Megaman OST
 # downloadAudio('https://www.youtube.com/watch?v=qzlUJk3-h_k')
@@ -38,43 +24,65 @@ def downloadAudio(url):
 # Overwatch OST
 downloadAudio('https://www.youtube.com/watch?v=DytVLscuUdI')
 
-# Hour:Min:Sec
-timestamp_pattern = r'(?:([0-9]?[0-9]):)?([0-5]?[0-9]):([0-5][0-9])'
-#same pattern as above but wrapped in ([])
-timestamp_pattern_wrapped = r'[\(|\[]?' + timestamp_pattern + r'[\)|\]]?'
-timestamp_reg = re.compile(timestamp_pattern)
-timestamp_wrapped_reg = re.compile(timestamp_pattern_wrapped)
-
-# Parse description and get a list of (timestamp, song name) tuples
-def get_timestamps(filename):
-    tuples = []
-    with open(tmpdir+'/'+'AUDIO.description') as f:
-        lines = f.read().splitlines()
-        for l in lines:
-            matched = timestamp_reg.search(l)
-            if matched:
-                timestamp = matched.group()
-                wrapped_timestamp = timestamp_wrapped_reg.search(l).group()
-                # Remove the timestamp from the line
-                songName = l.replace(wrapped_timestamp, "").strip()
-                tuples.append((timestamp, songName))
-    return tuples
-
 # Simple class to hold info about the OST
 class FullOst(object):
-    def __init__(self, description_file, audio_file, info_json_file):
-        self.audio_file = audio_file
+    def __init__(self, description_file, info_json_file):
         self.description_file = description_file
-        self.info_json_file = info_json_file
+        self.info = json.loads(info_json_file)
+        self.audio_file = self.info['_filename']
         self.timestamps_and_songs = self.__parse_timestamps_and_songs()
+
+    # Youtube-dl removes the timestamp, but often the description will wrap the timestamp in
+    # parens or square brackets.
+    def __scrub_song_name(self, song):
+        return song.replace('[]','').replace('()','')
 
     def __parse_timestamps_and_songs(self):
         return get_timestamps(self.description_file)
 
     # split single audio file into individual songs, saved at output_dir
     def splitOST(self, output_dir):
-        pass
+        chapters = self.info['chapters']
+        extension = self.info['extension']
+        fullsong = AudioSegment.from_file(self.audio_file, extension)
+        for chapter in chapters:
+            start_time = int(chapter['start_time']) * 1000
+            end_time =  int(chapter['end_time']) * 1000
+            song = fullsong[start_time:end_time]
+            song_name = self.__scrub_song_name(chapter['tile'])
+            song.export(os.path.abspath(output_dir + '/' + song_name), format="mp3")
+
 
 # Remove .m4a, .description, and .json files in tmp dir
 def cleanUpTempDir():
     pass
+
+def downloadAudio(url, download_dir=None):
+    if download_dir == None:
+        download_dir = os.getcwd()
+    ydl_opts = {
+        'writedescription': True,
+        'writeinfojson': True,
+        'outtmpl': download_dir + '/' + 'AUDIO.%(ext)s',
+        'logger': MyLogger(),
+        'format': 'bestaudio[ext=m4a]'
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+def main():
+    parser = argparse.ArgumentParser(description='Download and split Full OST from Youtube link')
+    parser.add_argument('url', type=str, help='youtube link to download from', required=True)
+    parser.add_argument('--outputdir', type=str, default=os.getcwd(), help='directory to output songs to', required=False)
+    args = parser.parse_args()
+
+    tmpdir=tempfile.mkdtemp()
+    downloadAudio(args.url, download_dir=tmpdir)
+    description_file="%s/%s" % (tmpdir,'AUDIO.description')
+    info_file="%s/%s" % (tmpdir,'AUDIO.info.json')
+    ost = FullOst(description_file,info_file)
+    ost.splitOST(args.outputdir)
+
+if __name__ == '__main__':
+    main()
