c72e77384f1a44c735388390af24f83395ff0430
Joe Wang <joewang@box.com>
2018-05-28 19:52:33 -0700

    Mutagen for metadata, fetch album art, get best quality (download vid then extract audio, gotta get that 192k 😩)

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
 import youtube_dl
 import tempfile
 import os
+import sys
 import re
 import json
 from pydub import AudioSegment
 import argparse
+import urllib
+from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3, APIC, error
+from mutagen.mp3 import MP3
 
 class MyLogger(object):
     def debug(self, msg):
@@ -16,41 +22,90 @@ class MyLogger(object):
     def error(self, msg):
         print(msg)
 
-
-
 # Megaman OST
 # downloadAudio('https://www.youtube.com/watch?v=qzlUJk3-h_k')
 
-# Overwatch OST
-downloadAudio('https://www.youtube.com/watch?v=DytVLscuUdI')
+
+# Simple class to hold individual song data and metadata
+class Song(object):
+    def __init__(self, data, song_name, album_name, track_num, coverfile=None):
+        self.data = data
+        self.title = song_name
+        self.album = album_name
+        self.track_num = track_num
+        self.coverfile = coverfile
+
+    def export(self, file_path, fileformat="mp3"):
+        self.data.export(file_path, format=fileformat)
+    
+    def update_metadata(self, file_path):
+        # Too lazy to read the spec, more efficient just to use ID3 to do
+        # textual metadata and album art, but ¯\_(ツ)_/¯
+        audio = EasyID3(file_path)
+        audio['title'] = self.title
+        audio['album'] = self.album
+        audio['tracknumber'] = self.track_num
+        audio.save()
+
+        if not self.coverfile:
+            audio = ID3(file_path)
+            with open(self.coverfile, 'wb') as f:
+                audio['APIC'] = APIC(
+                        encoding=3, # 3 is for utf-8
+                        mime='image/jpeg', # image/jpeg or image/png
+                        type=3, # 3 is for the cover image
+                        desc=u'Cover',
+                        data=f.read()
+                )
+            audio.save()
 
 # Simple class to hold info about the OST
 class FullOst(object):
     def __init__(self, description_file, info_json_file):
         self.description_file = description_file
-        self.info = json.loads(info_json_file)
+        with open(info_json_file,"r") as f:
+            data = f.read()
+            self.info = json.loads(data)
         self.audio_file = self.info['_filename']
-        self.timestamps_and_songs = self.__parse_timestamps_and_songs()
 
     # Youtube-dl removes the timestamp, but often the description will wrap the timestamp in
     # parens or square brackets.
     def __scrub_song_name(self, song):
         return song.replace('[]','').replace('()','')
 
-    def __parse_timestamps_and_songs(self):
-        return get_timestamps(self.description_file)
+    def fetch_album_art(self, directory):
+        urllib.request.urlretrieve(self.info['thumbnail'], "%s/art.jpeg" % (directory))
 
     # split single audio file into individual songs, saved at output_dir
     def splitOST(self, output_dir):
+        album_folder = "%s/%s" %(output_dir, self.info['fulltitle'])
+        try:
+            os.mkdir(album_folder)
+        except FileExistsError:
+            if not os.path.isdir(album_folder):
+                print("File %s already exists and is not a folder", album_folder)
+                sys.exit(1)
+
         chapters = self.info['chapters']
-        extension = self.info['extension']
-        fullsong = AudioSegment.from_file(self.audio_file, extension)
-        for chapter in chapters:
+        extension = self.info['ext']
+        fullOSTAudio = AudioSegment.from_file(self.audio_file, extension)
+
+        self.fetch_album_art(album_folder)
+
+        for i,chapter in enumerate(chapters):
             start_time = int(chapter['start_time']) * 1000
             end_time =  int(chapter['end_time']) * 1000
-            song = fullsong[start_time:end_time]
-            song_name = self.__scrub_song_name(chapter['tile'])
-            song.export(os.path.abspath(output_dir + '/' + song_name), format="mp3")
+            audio_segment = fullOSTAudio[start_time:end_time]
+            song_name = self.__scrub_song_name(chapter['title'])
+            tags = {'album': self.info['fulltitle'], 'track':i }
+
+            print("Song: " + song_name)
+            print("Folder: " + album_folder)
+
+            song_filename = "%s/%s.%s" % (album_folder, song_name, 'mp3')
+            song = Song(audio_segment, song_name, self.info['fulltitle'], i, "%s/art.jpeg" % (album_folder))
+            song.export(song_filename)
+            song.update_metadata(song_filename)
 
 
 # Remove .m4a, .description, and .json files in tmp dir
@@ -60,12 +115,25 @@ def cleanUpTempDir():
 def downloadAudio(url, download_dir=None):
     if download_dir == None:
         download_dir = os.getcwd()
+    # ydl_opts = {
+    #     'writedescription': True,
+    #     'writeinfojson': True,
+    #     'outtmpl': download_dir + '/' + 'AUDIO.%(ext)s',
+    #     'logger': MyLogger(),
+    #     'format': 'bestaudio[ext=m4a]'
+    # }
+
     ydl_opts = {
+        'format': 'best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
         'writedescription': True,
         'writeinfojson': True,
         'outtmpl': download_dir + '/' + 'AUDIO.%(ext)s',
         'logger': MyLogger(),
-        'format': 'bestaudio[ext=m4a]'
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -73,16 +141,18 @@ def downloadAudio(url, download_dir=None):
 
 def main():
     parser = argparse.ArgumentParser(description='Download and split Full OST from Youtube link')
-    parser.add_argument('url', type=str, help='youtube link to download from', required=True)
-    parser.add_argument('--outputdir', type=str, default=os.getcwd(), help='directory to output songs to', required=False)
+    parser.add_argument('url', type=str, help='youtube link to download from')
+    parser.add_argument('--outputdir', type=str, default=os.getcwd(), required=False, help='directory to output songs to')
+    parser.add_argument('--tmpdir', type=str, default=tempfile.mkdtemp(), required=False, help='staging directory for store full ost and metadata')
     args = parser.parse_args()
+    tmpdir= os.path.abspath(args.tmpdir)
+    outputdir= os.path.abspath(args.outputdir)
 
-    tmpdir=tempfile.mkdtemp()
-    downloadAudio(args.url, download_dir=tmpdir)
-    description_file="%s/%s" % (tmpdir,'AUDIO.description')
-    info_file="%s/%s" % (tmpdir,'AUDIO.info.json')
+    downloadAudio(args.url, download_dir=args.tmpdir)
+    description_file="%s/%s" % (args.tmpdir,'AUDIO.description')
+    info_file="%s/%s" % (args.tmpdir,'AUDIO.info.json')
     ost = FullOst(description_file,info_file)
-    ost.splitOST(args.outputdir)
+    ost.splitOST(outputdir)
 
 if __name__ == '__main__':
     main()
